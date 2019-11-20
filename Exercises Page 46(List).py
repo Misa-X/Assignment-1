2326de252e805897e7bdea028e41b3785b22c1ca
Misa-X <57937455+Misa-X@users.noreply.github.com>
2019-11-20 11:28:35 +0700

    Add files via upload

---
+++ Exercises Page 46(List).py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 14:14:36 2019
+
+@author: user
+"""
+
+guests = ["Shiraz", "Auman", "Aurelio", "Manuela", "Jhonaty"]
+for guest in guests:
+    print((guests[0]) + " please come to my party tomorrow night at my house")
+    print((guests[1]) + " please come to my party tomorrow night at my house")
+    print((guests[2]) + " please come to my party tomorrow night at my house")
+    print((guests[3]) + " please come to my party tomorrow night at my house")
+    print((guests[4]) + " please come to my party tomorrow night at my house")
+print("\n")
+    
+print((guests[4]) + " it's sad to hear that you'll not be able to come to my party")
+print("")
+guests = ["Shiraz", "Auman", "Aurelio", "Manuela", "Jhonaty"]
+guests.pop()
+print(guests)
+guests.insert(4, "John")
+print(guests)
+for guest in guests:
+    print((guests[0]) + " please come to my party tomorrow night at my house")
+    print((guests[1]) + " please come to my party tomorrow night at my house")
+    print((guests[2]) + " please come to my party tomorrow night at my house")
+    print((guests[3]) + " please come to my party tomorrow night at my house")
+    print((guests[4]) + " please come to my party tomorrow night at my house")
+print("\n")
+print("hey guests,i found a bigger dinning table so i'll invite more people")
+guests.insert(0, "Sam")
+guests.insert(3, "Steve")
+guests.append("Bob")
+print(guests)
+print("\n")
+for guest in guests:
+    print((guests[0]) + " please come to my party tomorrow night at my house")
+    print((guests[1]) + " please come to my party tomorrow night at my house")
+    print((guests[2]) + " please come to my party tomorrow night at my house")
+    print((guests[3]) + " please come to my party tomorrow night at my house")
+    print((guests[4]) + " please come to my party tomorrow night at my house")
+    print((guests[5]) + " please come to my party tomorrow night at my house")
+    print((guests[6]) + " please come to my party tomorrow night at my house")
+    print((guests[7]) + " please come to my party tomorrow night at my house")
+
+
+
+
+
+
+    
+     
